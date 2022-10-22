78965cda7890faf91c23818cdb8b3b0c943ee1a5
ItsZeus03 <82116305+ItsZeus03@users.noreply.github.com>
2022-10-21 21:42:40 -0500

    Add files via upload

---
+++ dias de la semana.py
@@ -0,0 +1,16 @@
+
+dia=str(input("que dias es "))
+if dia =="lunes":
+    print("inici de semana :(")
+elif dia=="martes":
+    print("alemnos ya dfaltan 3 dias para el fincho")
+elif dia=="martes":
+    print("alemnos ya dfaltan 3 dias para el fincho")
+elif dia=="maiercoles":
+    print("ya mirar de semana")
+elif dia=="jueves":
+    print("ya casi es sabado")
+elif dia=="viernes":
+    print("ya es viernes jijijijij")
+else:
+    print("ya es fincho")
---
+++ inicio Python.py
@@ -0,0 +1,15 @@
+#Hola Mundo
+print("Hola Python")
+
+
+
+# tipo de datos basicos 
+print(type("hola")) # Tipo str
+
+print(type(5)) # Tipo int
+
+print(type(5.6)) # Tipo float
+
+print(type(False)) # Tipo bool
+
+print(type(1+3j)) # Tipo
